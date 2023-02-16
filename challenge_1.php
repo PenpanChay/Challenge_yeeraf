2c4c7e12adada4b8b353c178ef038400c7614dc5
PenpanChay <67403002+PenpanChay@users.noreply.github.com>
2023-02-16 20:08:09 +0700

    Add files via upload

---
+++ challenge_1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+for ($x = 1; $x <= 100; $x++) {
+if ($x % 3 == 0 && $x % 5 == 0) {
+  echo "ThreeFive <br>";
+}
+else if ($x %3 ==0) {
+  echo "Three <br>";
+}
+else if ($x %5 ==0) {
+  echo "Five <br>";
+} else
+{
+  echo "$x <br>";
+  }
+}
+?>  
+
+</body>
+</html>
