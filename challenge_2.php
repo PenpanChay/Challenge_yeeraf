18f3f40c931d929f781823ab1d39cf0dcef7eecb
PenpanChay <67403002+PenpanChay@users.noreply.github.com>
2023-02-16 20:10:16 +0700

    Update challenge_2.php

--- challenge_2.php
+++ challenge_2.php
@@ -7,8 +7,8 @@ $nums = array(3, 1, 2, 3);
 $sums = 6;
 $output = "";
 
-for ($x = 0; $x < count($nums)-1; $x++)  {
-for ($y = $x+1; $y < count($nums); $y++)  {
+for ($x=0; $x<count($nums)-1; $x++)  {
+for ($y=$x+1; $y<count($nums); $y++)  {
 if($nums[$x] + $nums[$y] == $sums){
   $output = "$x,$y";
   echo "$output";
